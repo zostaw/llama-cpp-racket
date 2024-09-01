efd411c80bc231667c8d0c8c02a0f45b49cfea1b
zostaw <zostaw@users.noreply.github.com>
2024-09-01 21:45:51 +0200

    model is loaded, but I have no control over it. I get a pinter, but that's about it. I'm not sure if the object is still persistent at that point. The model clearly initializes, though, we get the output.

--- wrapper/llama.cpp
+++ wrapper/llama.cpp
@@ -5,8 +5,22 @@ struct llama_model_params * model_params_init()
     *params = llama_model_default_params();
     return params;
 }
+
 void model_params_free(struct llama_model_params * params)
 {
     free(params);
 }
 
+void * model_init_from_file(const char * file_path, struct llama_model_params params)
+{
+    //struct llama_model * model = (struct llama_model *) malloc(sizeof(struct llama_model));
+    struct llama_model * model = llama_load_model_from_file(file_path, params);
+    void* model_ptr = &model;
+    printf("CPP side struct: %p\n", model);
+    printf("CPP side void: %p\n", model_ptr);
+    return model_ptr;
+}
+
+void model_free(struct llama_model ** model) {
+    free(*model);
+}
--- wrapper/llama.h
+++ wrapper/llama.h
@@ -1,3 +1,5 @@
 // wrapper part (added from external script)
 struct llama_model_params * model_params_init();
 void model_params_free(struct llama_model_params * params);
+void * model_init_from_file(const char * file_path, struct llama_model_params params);
+void model_free(struct llama_model ** model);
