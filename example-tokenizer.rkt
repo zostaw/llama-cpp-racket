b6bed494713aa29e2f15b03c3267842a2b0e85a5
zostaw <zostaw@users.noreply.github.com>
2024-09-10 11:49:05 +0200

    Formatting

--- example-tokenizer.rkt
+++ example-tokenizer.rkt
@@ -4,12 +4,18 @@
 (require ffi/unsafe
          ffi/unsafe/define)
 
+
+
+
 ;; Initialize
+; complete path, because raw string doesn't work with llama-load-model-from-file if called from DrRacket
 (define model-path (path->complete-path "t5-v1_1-xxl-encoder-Q5_K_M.gguf"))
 (define model-params (llama-model-default-params))
 (define model (llama-load-model-from-file model-path model-params))
+
 (define ctx-params (llama-context-default-params))
 (define ctx (llama-new-context-with-model model ctx-params))
+
 (define add-special (llama-add-bos-token model))
 (define tokenize
    (tokenizer model
@@ -22,6 +28,8 @@
                    #t))
 
 
+
+
 ;; Tokenize from input text
 (define text "This is some random text to tokenize")
 (define-values (tokens tokens-len) (tokenize text))
@@ -30,6 +38,8 @@
     (define piece (token-to-piece token))
     (displayln (format "token: ~a, piece: \"~a\"" token piece ))))
   
+
+
 
 ;; Deallocate
 (llama-free-context ctx)
